f5c8436e838b7a44f905767cd648106ef6eb40d6
Javier <javier.garcia.ta@udima.es>
2022-03-18 12:23:03 +0100

    Created script to power on an off the bulbs with a button

---
+++ room.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+
+import os
+from PyP100 import PyL530
+
+from dotenv import load_dotenv
+
+
+class rooms:
+    ## Methods and function for a group of bulbs
+
+    def __init__(self):
+        load_dotenv()  # take environment variables from .env.
+
+        # Create list of bulbs
+        self.bulbs = []
+        self.bulbs.append(
+            PyL530.L530("10.0.0.254", os.getenv("MAIL"), os.getenv("PASS"))
+        )
+        self.bulbs.append(
+            PyL530.L530("10.0.0.253", os.getenv("MAIL"), os.getenv("PASS"))
+        )
+        self.bulbs.append(
+            PyL530.L530("10.0.0.252", os.getenv("MAIL"), os.getenv("PASS"))
+        )
+
+    def login(self):
+        """Login all bulbs"""
+
+        # Login bulbs
+        for bulb in self.bulbs:
+            bulb.handshake()  # Creates the cookies required for further methods
+            bulb.login()  # Sends credentials to the plug and creates AES Key
+            # and IV for further methods
+
+    def poweron(self):
+        """Power on all bulbs"""
+        for bulb in self.bulbs:
+            bulb.turnOn()
+
+    def poweroff(self):
+        """Power off all bulbs"""
+        for bulb in self.bulbs:
+            bulb.turnOff()
